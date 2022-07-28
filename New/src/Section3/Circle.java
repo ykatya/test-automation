1e8b81e8584844e2d131b2915488f0e45b54d8ba
ykatya <eekat19@gmail.com>
2022-07-28 12:56:40 +0300

    Commit section3

--- New/src/Section3/Circle.java
+++ New/src/Section3/Circle.java
@@ -1,10 +1,11 @@
 package Section3;
 
 public class Circle {
+    final public static double Math_PI = 3.14;
     public static void calculatearea(double radius){
-        System.out.println("The area is "+ Math.PI * ( radius * radius ));
+        System.out.println("The area is "+ Math_PI * ( radius * radius ));
     }
     public static void calculatecircumference(double radius){
-        System.out.println("The circumference is " + Math.PI * 2*radius);
+        System.out.println("The circumference is " + Math_PI * 2*radius);
     }
 }
